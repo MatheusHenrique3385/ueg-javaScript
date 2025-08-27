87eaf029de7dbb169230e7ae582f0a56946bf4c1
Matheus Henrique <math.henri.soares@gmail.com>
2025-08-26 21:05:59 -0300

    Create aula1.js

---
+++ aula1.js
@@ -0,0 +1,37 @@
+// NUMBER
+
+/*console.log(2+4);
+console.log(5-3);
+console.log(4*3);
+console.log(5/2);
+console.log(5/2);
+console.log(5/(4+2));
+console.log(10*"maria");*/
+
+// STRING
+
+/*console.log(typeof"maria");
+console.log(typeof'maria');
+console.log(typeof `maria`);*/
+
+// BOOLEAN
+
+/*console.log(5>7);
+console.log(5<7);
+console.log(5<5);
+console.log(5<=7);
+console.log(5==5);
+console.log(7=="7");
+console.log(7==="7");
+console.log(7!="9");
+console.log(9!="9");
+console.log(7!=="9");
+console.log(5>3&&9!="9");
+*/
+
+//Funções Aritimeticas
+
+console.log(Math.sqrt(9));
+console.log(Math.random());
+console.log(Math.PI);
+console.log(Math.pow(4,8));
